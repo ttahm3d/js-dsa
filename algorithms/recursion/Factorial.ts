e731a2f8e343cb8000423cd1691b44015fd5af0a
ttahm3d <ttahm3d@gmail.com>
2022-12-25 21:38:41 +0530

    [feat] added recursive solution for factorial

---
+++ algorithms/recursion/Factorial.ts
@@ -0,0 +1,26 @@
+/**
+ * Given a integer n find the factorial of that number
+ */
+
+function recursiveFactorial(n: number): number {
+  if (n < 0) return -1;
+  else if (n === 0) return 1;
+  else return n * recursiveFactorial(n - 1);
+}
+
+console.log(recursiveFactorial(5));
+
+/**
+ *
+ * consider the number 5
+ *
+ * 5! => 5 * 4!
+ * 5! => 5 * 4 * 3!
+ * 5! => 5 * 4 * 3 * 2!
+ * 5! => 5 * 4 * 3 * 2 * 1!
+ * 5! => 5 * 4 * 3 * 2 * 1 * 0!
+ *
+ * So for 5 =>  5 times
+ *
+ * O(n)
+ */
