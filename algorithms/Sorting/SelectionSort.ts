e15a913a295dcacb043fab5b7c7da676315cce86
ttahm3d <ttahm3d@gmail.com>
2023-09-20 21:20:20 +0530

    [feat] - updated with timecomplexity

--- algorithms/Sorting/SelectionSort.ts
+++ algorithms/Sorting/SelectionSort.ts
@@ -2,6 +2,11 @@
  * Selection sort - sorts the array in increasing order
  * It selects the minmum of array from positions 1 - n-1, 2 - n-2 .....
  * in each pass and places them at the indices 0, 1, 2...so on till the array is sorted
+ *
+ * Complexity
+ *
+ * For all cases - O(n2)
+ *
  * @param nums array of numbers
  * @returns Sorted array of numbers
  */
@@ -11,16 +16,12 @@ function selectionSort(nums: number[]): number[] {
     let minIndex = i;
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[j] < nums[minIndex]) minIndex = j;
-      let temp = nums[i];
-      nums[i] = nums[minIndex];
-      nums[minIndex] = temp;
     }
+    let temp = nums[i];
+    nums[i] = nums[minIndex];
+    nums[minIndex] = temp;
   }
   return nums;
 }
 
 console.log(selectionSort([4, -5, -2, 12, 9]));
-
-/**
- *
- */
